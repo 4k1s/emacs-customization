c4201b485c3840020f8fa0367143cb23122e2a46
akis <akis@bcschain.io>
2019-11-17 12:42:29 +0700

    trimming

--- init.el
+++ init.el
@@ -31,33 +31,10 @@
     (## web-mode virtualenv multi-web-mode js2-mode jedi go-complete flymake-shell flymake-gjshint flymake-css flatland-black-theme ecb company-php ac-php ac-html-bootstrap ac-html ac-capf ac-anaconda))))
 
 ;; set default working directory
-(setq default-directory "~/coding")
+(setq default-directory "~/")
 
 ;; auto-complete
 (ac-config-default)
-
-;; Open .v files with Proof General's Coq mode
-(load "~/.emacs.d/lisp/PG/generic/proof-site")
-
-(defadvice js-jsx-indent-line (after js-jsx-indent-line-after-hack activate)
-  "Workaround sgml-mode and follow airbnb component style."
-  (save-excursion
-    (beginning-of-line)
-    (if (looking-at-p "^ +\/?> *$")
-        (delete-char sgml-basic-offset))))
-
-;; activate js2-mode for .js files
-;;(autoload 'js2-mode "js2" "js2 mode" t)
-;;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-;; node auto-complete
-(add-to-list 'load-path "~/.emacs.d/node-ac")
-(require 'node-ac-mode)
-(setq node-ac-node-modules-path "/usr/lib/node_modules/npm/node_modules")
-
-;; for qml
-(autoload 'qml-mode "qml-mode" "Editing Qt Declarative." t)
-(add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
 
 ;; neotree
 (add-to-list 'load-path "~/.emacs.d/neotree")
